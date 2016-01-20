700db3208e753f1df3325386a09555825dad52a4
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-01-20 12:13:58 -0500

    receives basic yelp data

--- app/models/yelp.rb
+++ app/models/yelp.rb
@@ -1,24 +1,34 @@
 require 'yelp'
 
-Yelp.client.configure do |config|
-  config.consumer_key = ENV['YOUR_CONSUMER_KEY']
-  config.consumer_secret = ENV['YOUR_CONSUMER_SECRET']
-  config.token = ENV['YOUR_TOKEN']
-  config.token_secret = ENV['YOUR_TOKEN_SECRET']
+class Yelp
+
+  def initialize
+
+    Yelp.client.configure do |config|
+      config.consumer_key = ENV['YOUR_CONSUMER_KEY']
+      config.consumer_secret = ENV['YOUR_CONSUMER_SECRET']
+      config.token = ENV['YOUR_TOKEN']
+      config.token_secret = ENV['YOUR_TOKEN_SECRET']
+    end
+
+    url = "https://api.yelp.com/v2/search?term=food&location=San+Francisco"
+
+    response = HTTParty.get(url)
+
+    # instantiating abv, style, and photo_url by parsing through the JSON response
+  end
+  # Yelp.client.search('San Francisco', { term: 'food' })
+
+
+  # # search
+  # response = Yelp.client.search('San Francisco')
+  #
+  # response.businesses
+  # # [<Business 1>, <Business 2>, ...]
+  #
+  # response.businesses[0].name
+  # # "Kim Makoi, DC"
+  #
+  # response.businesses[0].rating
+  # 5.0
 end
-
-
-# Yelp.client.search('San Francisco', { term: 'food' })
-
-
-## search
-response = Yelp.client.search('San Francisco')
-
-response.businesses
-# [<Business 1>, <Business 2>, ...]
-
-response.businesses[0].name
-# "Kim Makoi, DC"
-
-response.businesses[0].rating
-# 5.0
