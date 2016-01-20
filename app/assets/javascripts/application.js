e839fd29f659e83809ba6b2d5c69885e8e11e5f8
Qadriyyyah Thomas <qmadinah@gmail.com>
2016-01-20 11:18:43 -0500

    add leaflet to index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,18 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require jquery
 //= require materialize-sprockets
+//= require_tree .
+$(document).ready(function(){
+var map = L.map('map').setView([38.9038829, -77.0360032], 15);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+
+    // Replace the below properties with your `Map ID` and `Default Public Token` values you saved earlier, respectively.
+    id: 'qadriyyah119.oo9l9c9m',
+    accessToken: 'pk.eyJ1IjoicWFkcml5eWFoMTE5IiwiYSI6ImNpajM4aW9jdzAwNDh0emtubHhkMDRiam0ifQ.fxo0A5_QYpfwsPlHMtU0VA'
+}).addTo(map);
+});
