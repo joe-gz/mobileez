aad28cf53ccd6715fc4c5007f175159de7f7a0b2
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-01-21 17:38:43 -0500

    adds map functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,78 @@
 //= require materialize-sprockets
 //= require_tree .
 $(document).ready(function(){
-var map = L.map('map').setView([38.9038829, -77.0360032], 15);
+  var map = L.map('map').setView([38.9038829, -77.0360032], 15);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
 
     // Replace the below properties with your `Map ID` and `Default Public Token` values you saved earlier, respectively.
-    id: 'qadriyyah119.oo9l9c9m',
-    accessToken: 'pk.eyJ1IjoicWFkcml5eWFoMTE5IiwiYSI6ImNpajM4aW9jdzAwNDh0emtubHhkMDRiam0ifQ.fxo0A5_QYpfwsPlHMtU0VA'
-}).addTo(map);
+    id: "ID",
+    accessToken: "ACCESS_TOKEN"
+  }).addTo(map);
+
+  // map.locate({ setView: true, enableHighAccuracy: true }).on('locationfound', function(e){
+  //   console.log( "Location detected. Lat: " + e.latitude + ", Long: " + e.longitude );
+  //   map.panTo([e.latitude, e.longitude], {animate: true, duration: 5.0});
+  // }).on('locationerror', function(e){
+  //   console.log("Location not detected.");
+  // });
+
+  var searchArray = []
+  var coords = []
+  var travelLineCoords = []
+
+    event.preventDefault();
+    var keyword = $("input[name='name']").val();
+    var url = "http://api.opencagedata.com/geocode/v1/json?q="+keyword+"&key=API_KEY"
+    $.ajax({
+      url: url,
+      type: "GET",
+      dataType: "json"
+    }).done ( function(response){
+      // call movie function below to append movie titles
+      searchArray.push(response.results[0])
+      addressRequest(response.results[0]);
+    }).fail ( function (){
+      console.log("fail");
+    }).always( function(){
+      console.log("Something happens");
+    })
+
+  var addressRequest = function (response){
+    var lat = parseFloat(response.geometry.lat);
+    var long = parseFloat(response.geometry.lng);
+    var comment = $("input[name='comment']").val();
+    var locationDiv = "<div>Location: "+response.formatted+"</div>";
+    var coordinateDiv = "<div>Coordinates: "+[lat,long]+"</div>";
+    var commentDiv = "<div>Comment: "+comment+"</div>";
+
+    var travelLine = {
+      "type": "Feature",
+      // Set geometry type here.
+      "geometry": {
+        "type": "MultiLineString",
+        "coordinates":
+        travelLineCoords
+      }
+    }
+
+    coords.push([long,lat]);
+    if (coords.length>=2){
+      for (var i = 0; i < coords.length-1; i++){
+        travelLineCoords.push([coords[i],coords[i+1]])
+      }
+      L.geoJson( travelLine ).addTo( map );
+    }
+
+    var clickPopup = L.popup();
+
+    L.marker([lat,long]).addTo(map).bindPopup(clickPopup)
+    clickPopup
+    .setLatLng([lat,long])
+    .setContent(locationDiv+coordinateDiv+commentDiv)
+    .openOn(map);
+  }
+
 });
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,9 +2,15 @@ class LocationsController < ApplicationController
 
   def index
     if params[:name]
-      @yelp = Yelp.client.search(params[:name])
+      @yelp = Yelp.client.search(params[:name], { term: 'food' })
     end
 
+  end
+
+  def show
+    if params[:id]
+      @yelp = Yelp.client.business(params[:id])
+    end
   end
 
   def create
