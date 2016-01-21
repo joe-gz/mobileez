5329fcd7c9b8f5748f5a3fded153d1c9c90aea84
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2016-01-21 15:42:32 -0500

    adds search

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,25 +1,17 @@
 class LocationsController < ApplicationController
 
   def index
-    @response = Location.new
+    if params[:name]
+      @yelp = Yelp.client.search(params[:name])
+    end
+
   end
 
   def create
-    # if params[:name]
-    @yelp = Yelp.client.search(params[:location][:name])
-    @response = Location.create(name:@yelp.businesses[0].name)
-    # else
-    #   @yelp = Yelp.client.search('Washington DC')
-    #   @response = Location.create(name:@yelp.businesses[0].name)
-    # end
-    redirect_to "/"
+
   end
 
   private
-
-  def location_params
-    params.require(:location).permit(:name)
-  end
 
 
 end
