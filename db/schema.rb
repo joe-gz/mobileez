309a1948159daba459d5aa1dd04c17bfd306f713
Josh Cohen <jacohen10@EMAIL.COM>
2016-01-25 20:03:01 -0500

    add jquery plugin for ratings. multiple scores still in progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122181519) do
+ActiveRecord::Schema.define(version: 20160126001401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 20160122181519) do
     t.integer "score"
     t.string  "location_id"
     t.integer "user_id"
+    t.integer "entrance_score"
+    t.integer "table_score"
+    t.integer "bathroom_score"
+    t.integer "parking_score"
   end
 
   create_table "users", force: :cascade do |t|
