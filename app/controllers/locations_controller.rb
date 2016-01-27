a54ed2051d33058e859eee38fc6d5362c5f4a828
Josh Cohen <jacohen10@EMAIL.COM>
2016-01-27 10:37:28 -0500

    add scores

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,6 +10,7 @@ class LocationsController < ApplicationController
   def show
     @review = Review.new
     @reviews = Review.all.where(location_id: params[:id])
+    @score = (@reviews.sum("parking_score", "entrance_score") + @reviews.sum("entrance_score") + @reviews.sum("table_score") + @reviews.sum("bathroom_score"))/@reviews.all.count
     if params[:id]
       @yelp = Yelp.client.business(params[:id])
     end
